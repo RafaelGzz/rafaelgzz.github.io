45c3ec869a72484dc28badecba60f65d766019af
RafaelGzz <ragl122778@gmail.com>
2021-06-17 11:11:33 -0500

    Bug Fix

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const User = require('../models/user');
 const { generarJWT } = require('../helpers/jwt');
 
 const newUser = async(req, res) => {
