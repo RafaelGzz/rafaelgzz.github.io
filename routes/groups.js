62214581ee0cf4c472d3215591f3a82dfc95102c
Rafael <ragl122778@gmail.com>
2021-06-19 21:47:03 -0500

    Added group endpoint

--- routes/groups.js
+++ routes/groups.js
@@ -4,11 +4,6 @@ const { validateFields } = require('../middlewares/validate-fields');
 const { validateJWT } = require('../middlewares/validate-jwt');
 
 
-router.post('/', [
-    check('name', 'El nombre es obligatorio').not().isEmpty(),
-    check('users', 'Los usuarios son obligatorios').not().isEmpty(),
-    validateJWT,
-    validateFields
-], createGroup);
+router.post('/', validateJWT, createGroup);
 
 module.exports = router;
