7ad042fd15bfeda5822c695dab947a7f1df2ddfa
RafaelGzz <ragl122778@gmail.com>
2021-06-24 14:09:50 -0500

    Bug fixes

--- middlewares/validate-fields.js
+++ middlewares/validate-fields.js
@@ -7,7 +7,7 @@ const validateFields = (req, res, next) => {
         console.log(err);
         return res.status(400).json({
             ok: false,
-            msg: err.errors.toList()
+            msg: err.errors
         });
     }
     next();
