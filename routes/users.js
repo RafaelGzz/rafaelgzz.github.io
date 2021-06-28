cc2d6fb8cb621ae80cb51320d43d20fd6d487f5c
Rafael <ragl122778@gmail.com>
2021-06-27 22:27:40 -0500

    Bug fix

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,14 @@ const { check } = require('express-validator');
 const router = require('express').Router();
 
 const { getUsers, updateUser } = require('../controllers/users_controller');
+const { validateFields } = require('../middlewares/validate-fields');
 const { validateJWT } = require('../middlewares/validate-jwt');
 
 router.get('/', validateJWT, getUsers);
 router.post('/update', [
     check('imageUrl').not().isEmpty(),
     check('imageUrl').isURL(),
+    validateFields,
     validateJWT
 ], updateUser);
 
