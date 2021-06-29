71d32b06fbebfc464615f8d3311aee5034cc331d
RafaelGzz <ragl122778@gmail.com>
2021-06-29 13:53:53 -0500

    bug fix

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -22,7 +22,7 @@ const saveMessage = async (payload) => {
     try {
         const message = new Message(payload);
         await message.save();
-        const conversation = Conversation.findById(payload.conversation);
+        const conversation = Conversation.findById(message.conversation);
         conversation.messages.push(message);
         await conversation.save();
 
