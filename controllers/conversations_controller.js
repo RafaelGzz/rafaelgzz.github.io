8434bc52faa94f855c0978d98c84bb10f0015bf2
Rafael <ragl122778@gmail.com>
2021-06-29 23:28:49 -0500

    bug fix

--- controllers/conversations_controller.js
+++ controllers/conversations_controller.js
@@ -22,7 +22,7 @@ const createConversation = async(req, res) => {
 
 const getConversations = async(req, res) => {
 
-    const conversations = await Conversation.find({ sender: req.uid }).populate("receiver", ["name", "imageUrl"]).populate("messages");
+    const conversations = await Conversation.find({ sender: req.uid }).populate("receiver", ["name", "imageUrl"]).populate("messages", ["message", "updatedAt"]);
 
     res.json({
         ok: true,
