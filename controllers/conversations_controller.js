be2c577776f748eed8e39a3d5ef350de9487af97
RafaelGzz <ragl122778@gmail.com>
2021-07-01 11:21:39 -0500

    return conversations by newer

--- controllers/conversations_controller.js
+++ controllers/conversations_controller.js
@@ -1,6 +1,6 @@
 const Conversation = require('../models/conversation');
 
-const createConversation = async(req, res) => {
+const createConversation = async (req, res) => {
 
     try {
         const conversation = await Conversation(req.body);
@@ -20,9 +20,14 @@ const createConversation = async(req, res) => {
     }
 }
 
-const getConversations = async(req, res) => {
+const getConversations = async (req, res) => {
 
-    const conversations = await Conversation.find({ sender: req.uid }).populate("receiver", ["name", "imageUrl"]).populate("messages", ["message", "updatedAt"]);
+    const conversations = await Conversation.find({ 
+        sender: req.uid 
+    })
+    .populate("receiver", ["name", "imageUrl"])
+    .populate("messages", ["message", "updatedAt"])
+    .sort({ updatedAt: 'desc' });
 
     res.json({
         ok: true,
